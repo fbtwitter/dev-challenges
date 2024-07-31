1214ff7811f8f9b424f5ccf86aa036b1c009213c
Reza Fauzi Augusdi <rzaugusdi354@gmail.com>
2024-08-01 00:13:35 +0700

    upgrade astro also add react and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 import vercelServerless from '@astrojs/vercel/serverless';
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercelServerless({
-    webAnalytics: { enabled: true }
+    webAnalytics: {
+      enabled: true
+    }
   }),
+  integrations: [react(), tailwind()]
 });
