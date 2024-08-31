9350b708f504578b480f2dadef57a498e57fef59
Reza Fauzi Augusdi <rzaugusdi354@gmail.com>
2024-08-31 23:39:35 +0700

    revert changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import vercelServerless from '@astrojs/vercel/serverless';
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
@@ -6,5 +7,10 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercelServerless({
+    webAnalytics: {
+      enabled: true
+    }
+  }),
   integrations: [react(), tailwind()]
 });
