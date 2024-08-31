9f795961d478a51b26aa55c9daa61f3fbc3a26c5
Reza Fauzi Augusdi <rzaugusdi354@gmail.com>
2024-08-31 23:38:29 +0700

    remove web analytic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import vercelServerless from '@astrojs/vercel/serverless';
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
@@ -7,10 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercelServerless({
-    webAnalytics: {
-      enabled: true
-    }
-  }),
   integrations: [react(), tailwind()]
 });
